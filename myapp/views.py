9b5453ca5335036f880c5a5c348525da4e4eaf8d
saritafai <sarita.fai@mail.kmutt.ac.th>
2021-05-29 00:03:32 +0700

    motify login

--- myapp/views.py
+++ myapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def hello(request):
     return render(request, 'index.html')
+
+def Login(request):
+    return render(request, 'login.html')
